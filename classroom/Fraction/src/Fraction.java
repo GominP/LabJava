7538efebef6caa428503f96b2271e7553b572299
Gomin <teelak1113@gmail.com>
2019-08-22 12:55:50 +0700

    Create classroom and lab1 Fraction

---
+++ classroom/Fraction/src/Fraction.java
@@ -0,0 +1,27 @@
+public class Fraction {
+    private int  numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denoinator) {
+        this.numerator = numerator;
+        this.denominator = denoinator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+    public Fraction add(Fraction other){
+        int p = numerator*other.getDenominator()+other.getNumerator()* denominator;
+        int q = denominator * other.getDenominator();
+        return  new Fraction(p,q);
+
+    }
+    @Override
+    public String toString(){
+        return numerator+"/"+denominator;
+    }
+}
